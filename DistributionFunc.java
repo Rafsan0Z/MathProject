8cb4a76b9823840c76a8b3ca82b51835b36f2997
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-11-12 14:43:56 -0500

    New Changes

--- DistributionFunc.java
+++ DistributionFunc.java
@@ -40,15 +40,8 @@ public static int rthPowerNum(int base, int r){
 }
 
 public static int[] rthPowerDatabase(int power, int start, int end) {
-	int base = 1;
-	for(int i = 1; i <= start ; i++) {
-		if(rthPowerNum(base,power) < start) {
-			base++;
-		}
-		if(rthPowerNum(base,power) > start) {
-			i = start + 1;
-		}
-	}
+	double data = Math.sqrt(start);
+	int base = (int)data;
 	int[] result = new int[end-start+1];
 	int bindex = 0;
 	for(int i = start; i <= end  ; i++) {
@@ -85,87 +78,107 @@ public static int rthPowerCount(int power, int start, int end) {
 	return result.length;
 }
 
-public static int[][] DistribFunc(int start, int group, int size, int power) {
-	int end = start + group*size;
-	int[][] result = new int[group][];
-	for (int i= 1; i <= group; i++) {
-		int first = (i-1)*size + 1;
-		int last = i*size;
-		result[i-1] = rthPowerDatabase(power,first,last);
-	} 
-	return result;
-}
+//public static int[][] DistribFunc(int start, int group, int size, int power) {
+//	int end = start + group*size;
+//	int[][] result = new int[group][];
+//	for (int i= 1; i <= group; i++) {
+//		int first = (i-1)*size + 1;
+//		int last = i*size;
+//		result[i-1] = rthPowerDatabase(power,first,last);
+//	} 
+//	return result;
+//}
 
-public static int DistribStat(int start, int group, int size, int power) {
+public static double DistribStat(int start, int group, int size, int power) {
 	int num = 0;
-	int counter = 0;
+	int total = start + group*size - 1;
 	for(int i = 1; i <= group ; i++) {
 		int first = start + (i-1)*size;
 		int end  = i*size;
-		if (rthPowerCount(power,first,end) == 0) {
+		if (rthPowerCount(power,first,end) > 0) {
 			num++;
-			counter = i;
 		}
 	}
-	return num ;	
+	return ((double)num / (double)group)*100;
 }
 
-public static boolean isPrime(int num) {
-int score = 0;
-for(int i = 2; i < num; i++) {
-	if (num % i != 0) {
-		score++;
-	}
-}
-if (score == num - 2) {
-	return true;
-}
-else {
-	return false;
-}
-}
+//public static int MaxNum(int n, int size, int start, int first) {
+	
+//}
 
-public static int[] PrimeList(int start, int end) {
-int count = 0;
-for(int i = start; i <= end; i++) {
-	if (isPrime(i)) {
-		count++;
-	}
-}
-int[] result = new int[count];
-int index = 0;
-for(int i = start; i <= end; i++) {
-	if(isPrime(i)) {
-		result[index] = i;
-		index++;
-	}
-}
-return result;
-}
 
-public static int PrimePosition(int start,int end,int prime) {
-int[] database = PrimeList(start,end);
-int pos = 0;
-for(int i =0; i<database.length ; i++) {
-	if(database[i] == prime) {
-		pos = i;
-		i = database.length;
-	}
-}
-return pos;
-}
+
+//public static boolean isPrime(int num) {
+//int score = 0;
+//for(int i = 2; i < num; i++) {
+//	if (num % i != 0) {
+//		score++;
+//	}
+//}
+//if (score == num - 2) {
+//	return true;
+//}
+//else {
+//	return false;
+//}
+//}
+
+//public static int[] PrimeList(int start, int end) {
+//int count = 0;
+//for(int i = start; i <= end; i++) {
+//	if (isPrime(i)) {
+//		count++;
+//	}
+//}
+//int[] result = new int[count];
+//int index = 0;
+//for(int i = start; i <= end; i++) {
+//	if(isPrime(i)) {
+//		result[index] = i;
+//		index++;
+//	}
+//}
+//return result;
+//}
+
+//public static double PrimeChance(int start, int group, int size) {
+//int end = start + group*size - 1;
+//int num = 0;
+//int total = group*size;
+//for(int i = 1; i <= group; i++) {
+//	int first = (i-1)*size + start;
+//	int last = i*size;
+//	int[] database = PrimeList(first,last);
+//	if(database.length > 0) {
+//		num++;
+//	}
+//}
+//	return ((double)num / (double)group)*100;
+//} 
+
+//public static int PrimePosition(int start,int end,int prime) {
+//int[] database = PrimeList(start,end);
+//int pos = 0;
+//for(int i =0; i<database.length ; i++) {
+//	if(database[i] == prime) {
+//		pos = i;
+//		i = database.length;
+//	}
+//}
+//return pos;
+//}
 
 public static void main(String[] args){
 
 //System.out.println(rthPowerNum(3,2));
-System.out.println(rthPowerCount(2,141,150));
-System.out.println(DistribStat(1,1500,10,2));
+//System.out.println(rthPowerCount(2,141,150));
+System.out.println(DistribStat(1,99999999,99999999,2));
 //PrintArray(DistribFunc(1,1500,10,2));
-System.out.println("The Number 97 is Prime? " + isPrime(97));
-printArray(rthPowerDatabase(2,141,150));
-//System.out.println(rthPowerDatabase(2,141,150));
-printArray(PrimeList(20,140));
-System.out.println("The Position of Prime 97 between 20 and 140 is: " + PrimePosition(20,140,97));
+//System.out.println("The Number 9 is Prime? " + isPrime(9));
+//printArray(rthPowerDatabase(2,141,150));
+//System.out.println(PrimeChance(1,50,2));
+//printArray(PrimeList(20,140));
+//System.out.println("The Position of Prime 97 between 20 and 140 is: " + PrimePosition(20,140,97));
 }
 
 }
